f5bc7d738dd0f38fc91754f025ba8d8bc4cca6c0
Hyper <34012267+hyperbx@users.noreply.github.com>
2024-08-07 18:22:44 +0100

    Use average frame rate (fixes variable frame rate videos)

--- 8mb.ps1
+++ 8mb.ps1
@@ -202,7 +202,7 @@ function GetSourceFPS()
     $result = & $ffprobe -v error `
                          -select_streams v `
                          -of default=noprint_wrappers=1:nokey=1 `
-                         -show_entries stream=r_frame_rate `
+                         -show_entries stream=avg_frame_rate `
                          $Source
 
     $split = $result -split '/'
