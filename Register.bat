6abfd8e532111b1bf778eebdd8701ffa4b4f5bbf
Hyper <34012267+hyperbx@users.noreply.github.com>
2024-09-03 23:07:17 +0100

    Added 10MB preset

--- Register.bat
+++ Register.bat
@@ -20,19 +20,24 @@ reg add "%baseKey%" /ve /d "" /f
 reg add "%baseKey%" /v SubCommands /d "" /f
 reg add "%baseKey%" /v MUIVerb /d "Compress" /f
 
+reg delete "%shellKey%" /f
+
 reg add "%shellKey%\a_8MB" /ve /d "8 MB" /f
 reg add "%shellKey%\a_8MB\command" /ve /d "powershell -ExecutionPolicy Bypass -File \"%scriptPath%\" \"%%1\" 8 MB 0.5 24 -Shell" /f
 
-reg add "%shellKey%\b_25MB" /ve /d "25 MB" /f
-reg add "%shellKey%\b_25MB\command" /ve /d "powershell -ExecutionPolicy Bypass -File \"%scriptPath%\" \"%%1\" 25 MB 0.75 30 -Shell" /f
+reg add "%shellKey%\b_10MB" /ve /d "10 MB" /f
+reg add "%shellKey%\b_10MB\command" /ve /d "powershell -ExecutionPolicy Bypass -File \"%scriptPath%\" \"%%1\" 10 MB 0.5 30 -Shell" /f
 
-reg add "%shellKey%\c_50MB" /ve /d "50 MB" /f
-reg add "%shellKey%\c_50MB\command" /ve /d "powershell -ExecutionPolicy Bypass -File \"%scriptPath%\" \"%%1\" 50 MB 0.75 30 -Shell" /f
+reg add "%shellKey%\c_25MB" /ve /d "25 MB" /f
+reg add "%shellKey%\c_25MB\command" /ve /d "powershell -ExecutionPolicy Bypass -File \"%scriptPath%\" \"%%1\" 25 MB 0.75 30 -Shell" /f
 
-reg add "%shellKey%\d_100MB" /ve /d "100 MB" /f
-reg add "%shellKey%\d_100MB\command" /ve /d "powershell -ExecutionPolicy Bypass -File \"%scriptPath%\" \"%%1\" 100 MB 1.0 60 -Shell" /f
+reg add "%shellKey%\d_50MB" /ve /d "50 MB" /f
+reg add "%shellKey%\d_50MB\command" /ve /d "powershell -ExecutionPolicy Bypass -File \"%scriptPath%\" \"%%1\" 50 MB 0.75 30 -Shell" /f
 
-reg add "%shellKey%\e_Custom" /ve /d "Custom" /f
-reg add "%shellKey%\e_Custom\command" /ve /d "powershell -ExecutionPolicy Bypass -File \"%scriptPath%\" \"%%1\" -Prompt" /f
+reg add "%shellKey%\e_100MB" /ve /d "100 MB" /f
+reg add "%shellKey%\e_100MB\command" /ve /d "powershell -ExecutionPolicy Bypass -File \"%scriptPath%\" \"%%1\" 100 MB 1.0 60 -Shell" /f
+
+reg add "%shellKey%\f_Custom" /ve /d "Custom" /f
+reg add "%shellKey%\f_Custom\command" /ve /d "powershell -ExecutionPolicy Bypass -File \"%scriptPath%\" \"%%1\" -Prompt" /f
 
 ENDLOCAL
